8a117b149982e57f1054608b4fbcbaafd5b7a012
tuomaspelkone <tuomas@pelkonen.com>
2025-11-03 14:49:35 +0200

    todo4 valmis

--- todo1/todo/src/App.jsx
+++ todo1/todo/src/App.jsx
@@ -1,18 +1,49 @@
-import { useState } from 'react'
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios' 
+import Row from './components/Row'
+
+const url = "http://localhost:3001"
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
-  const addTask = () => {
-    setTasks([...tasks, task])
-    setTask('')
-}
+  useEffect(() => {
+  axios.get(url)
+      .then(response => {
+        setTasks(response.data)
+      })
+      .catch(error => {
+        alert(error.response.data ? error.response.data.message : error)
+      })
+  }, [])
+
+
+   const addTask = () => {
+ const newTask = { description: task }
+ axios.post(url + "/create", {task: newTask})
+ .then(response => {
+ setTasks([...tasks,response.data])
+ setTask('')
+ })
+ .catch(error => {
+ alert(error.response ? error.response.data.error.message : error)
+ })
+ }
+
+
   const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter(item => item !== deleted)
-    setTasks(withoutRemoved)
-}
+ axios.delete(url + "/delete/" + deleted)
+ .then(response => {
+ setTasks(tasks.filter(item => item.id !== deleted))
+ })
+ .catch(error => {
+ alert(error.response ? error.response.data.error.message : error)
+ })
+ }
+
+ 
 
   return (
     <div id="container">
@@ -29,24 +60,31 @@ function App() {
         }}
       />
 
-      <ul>
-        {
-        tasks.map(item => (
-            <li>
-              {item}
-              <button
-                className='delete-button'
-                onClick={() => deleteTask(item)}>
-                Delete
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+{/*}
+       <ul>
+    {
+    tasks.map(item => (
+        <li key={item.id}>
+          {item.description}
+          <button className='delete-button' onClick={() =>
+            deleteTask(item.id)}>Delete</button>
+        </li>
+      ))
+    }
+  </ul>
+*/}
+
+
+<ul>
+ {
+ tasks.map(item => (
+ <Row item={item} key={item.id} deleteTask={deleteTask} />
+ ))
+ }
+</ul>
+  </div>
   )
+
 }
-
-
 
 export default App
