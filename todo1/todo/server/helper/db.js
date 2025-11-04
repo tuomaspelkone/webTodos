8f752fa159895df6818bbd114bae0bbfdf78ca1e
tuomaspelkone <tuomas@pelkonen.com>
2025-11-04 10:49:21 +0200

    todo5 valmis

---
+++ todo1/todo/server/helper/db.js
@@ -0,0 +1,27 @@
+import pkg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const environment = process.env.NODE_ENV || 'development'
+
+const port = process.env.port
+
+const { Pool } = pkg
+
+const openDb = () => {
+ const pool = new Pool({
+ user: process.env.DB_USER,
+ host: process.env.DB_HOST,
+ database: environment === "development" ? process.env.DB_NAME :
+ process.env.TEST_DB_NAME,
+ password: process.env.DB_PASSWORD,
+ port: process.env.DB_PORT
+ })
+ return pool
+}
+
+const pool = openDb()
+
+export { pool }
+
---
+++ todo1/todo/server/routes/todoRouter.js
@@ -0,0 +1,58 @@
+import { pool } from '../helper/db.js'
+import { Router } from 'express'
+
+const router = Router()
+const port = process.env.PORT
+
+router.get('/', (req, res, next) => {
+ pool.query('SELECT * FROM task', (err, result) => {
+ if (err) {
+ return next(err)
+ }
+ res.status(200).json(result.rows || [])
+ })
+})
+
+/*
+router.listen(port, () => {
+ console.log(`Server is running on http://localhost:${port}`)
+})
+ */
+
+router.post('/create', (req, res, next) => {
+ const { task } = req.body
+
+    if (!task) {
+ return res.status(400).json({error: 'Task is required'})
+ }
+
+ pool.query('insert into task (description) values ($1) returning *', [task.description],
+    (err, result) => {
+    if (err) {
+ return next (err)
+ }
+
+ res.status(201).json({id: result.rows[0].id, description: task.description})
+ })
+
+})
+
+router.delete('/delete/:id', (req, res, next) => {
+ const { id } = req.params
+
+ console.log(`Deleting task with id: ${id}`)
+ pool.query('delete from task WHERE id = $1',
+ [id], (err, result) => {
+ if (err) {
+ return next(err)
+ }
+
+ if (result.rowCount === 0) {
+ return res.status(404).json({error: 'Task not found'})
+ }
+ 
+ return res.status(200).json({id:id})
+ })
+})
+
+export default router
